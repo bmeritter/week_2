c9c524f5db7d6aa4220c8d6b8b6c7a4d3da1e9e4
bmeritter <bme_ritter@foxmail.com>
2017-07-13 10:38:14 +0800

    complete print n asterisks on one line

--- src/Easy.java
+++ src/Easy.java
@@ -2,7 +2,7 @@
  * Created by ritter on 17-7-13.
  */
 public class Easy {
-  public static void main(String [] argv) {
+  public static void main(String[] argv) {
     System.out.println("*");
   }
 }
---
+++ src/Horizontal.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ritter on 17-7-13.
+ */
+public class Horizontal {
+  public static void main(String[] argv) {
+    int n = 8;
+    while (n > 0) {
+      System.out.print("*");
+      n--;
+    }
+  }
+}
