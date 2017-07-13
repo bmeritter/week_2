717a10f4fdd28162b3d19d7c7368b58c60424e6b
bmeritter <bme_ritter@foxmail.com>
2017-07-13 10:40:58 +0800

    complete print n lines, each with one asterisks

---
+++ src/Vertical.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ritter on 17-7-13.
+ */
+public class Vertical {
+  public static void main(String[] argv) {
+    int n = 3;
+    while (n > 0) {
+      System.out.println("*");
+      n--;
+    }
+  }
+}
