1c7eaec855859e775139fec424ca73c85122f778
bmeritter <bme_ritter@foxmail.com>
2017-07-20 09:20:32 +0800

    finish Isosceles Triangle

---
+++ src/IsoscelesTriangle.java
@@ -0,0 +1,18 @@
+/**
+ * Created by ritter on 17-7-20.
+ */
+public class IsoscelesTriangle {
+  public static void main(String[] argv) {
+    int n = 3;
+    for (int i = 1; i <= 3; ++i) {
+      for (int j = 1; j <= n - i; ++j) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j < 2 * i; ++j) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+  }
+}
